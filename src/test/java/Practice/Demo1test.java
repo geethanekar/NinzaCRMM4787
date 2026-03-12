85abe40d24b04860d30a45c5c661bdf5c99e81e3
santh <santh@DESKTOP-AGP8TCP.attlocal.net>
2026-03-11 22:31:48 -0400

    first time committing

---
+++ src/test/java/Practice/Demo1test.java
@@ -0,0 +1,15 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class Demo1test {
+	
+	@Test
+	public void CarTest() {
+		
+		
+		System.out.println("BMW");
+	}
+	
+
+}
---
+++ src/test/java/Practice/Demotest.java
@@ -0,0 +1,13 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class Demotest {
+	
+	@Test(groups = "Smoke")
+	public void BikeTest() {
+		
+		System.out.println("RE");
+	}
+
+}
